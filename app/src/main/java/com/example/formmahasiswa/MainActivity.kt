a2c292ff0cd4742d53f1ccdea2846e5bad3d237f
Valerinejsc <dewivalerine@gmail.com>
2026-04-07 08:10:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/formmahasiswa/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.formmahasiswa
+
+import android.os.Bundle
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val etNama = findViewById<EditText>(R.id.etNama)
+        val rgGender = findViewById<RadioGroup>(R.id.rgGender)
+        val cbMembaca = findViewById<CheckBox>(R.id.cbMembaca)
+        val cbCoding = findViewById<CheckBox>(R.id.cbCoding)
+        val cbOlahraga = findViewById<CheckBox>(R.id.cbOlahraga)
+        val btnTampilkan = findViewById<Button>(R.id.btnTampilkan)
+        val tvHasil = findViewById<TextView>(R.id.tvHasil)
+
+        // Efek Border Biru saat Nama Diisi
+        etNama.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (!s.isNullOrEmpty()) {
+                    etNama.setBackgroundResource(R.drawable.bg_edittext_focused)
+                } else {
+                    etNama.setBackgroundResource(R.drawable.bg_edittext_normal)
+                }
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
+        btnTampilkan.setOnClickListener {
+            val nama = etNama.text.toString()
+            val selectedGenderId = rgGender.checkedRadioButtonId
+
+            if (nama.isEmpty()) {
+                etNama.error = "Nama tidak boleh kosong"
+                Toast.makeText(this, "Nama tidak boleh kosong!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (selectedGenderId == -1) {
+                Toast.makeText(this, "Jenis kelamin harus dipilih!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val gender = findViewById<RadioButton>(selectedGenderId).text.toString()
+
+            val hobiList = mutableListOf<String>()
+            if (cbMembaca.isChecked) hobiList.add("Membaca")
+            if (cbCoding.isChecked) hobiList.add("Coding")
+            if (cbOlahraga.isChecked) hobiList.add("Olahraga")
+
+            val hobi = if (hobiList.isEmpty()) "Tidak ada hobi" else hobiList.joinToString(", ")
+
+            // Format Hasil agar rapi seperti di gambar (Bold label)
+            tvHasil.text = android.text.Html.fromHtml("""
+                <b>Nama</b> &nbsp;&nbsp;&nbsp;: $nama <br/>
+                <b>Kelamin</b> : $gender <br/>
+                <b>Hobi</b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: $hobi
+            """.trimIndent(), android.text.Html.FROM_HTML_MODE_LEGACY)
+        }
+    }
+}
